9b1333ea8715712595174c84d55f0540a0cad22d
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-03 16:21:17 +0800

    Solve b.3.1

--- Assignment 2/section_b/3_1/xor.js
+++ Assignment 2/section_b/3_1/xor.js
@@ -1,20 +1,22 @@
-var str = "Take every byte xor it with forty-two you will get the flag";
+var input = "49 59 18 1b 1a 1d 51 19 52 1b 4c 75 49 1e 44 75 59 5e 1a 58 19 75 47 19 59 59 1e 4d 19 59 75 5e";
+var splitted = input.split(" ");
+
 var char_codes = [];
 
-for (var i = 0; i < str.length; i++) {
-	char_codes[i] = str.charCodeAt(i);
+for (var i = 0; i < splitted.length; i++) {
+	char_codes[i] = parseInt(splitted[i], 16);
 }
 
 var xor_codes = [];
 
-for (i = 0; i < str.length; i++) {
+for (i = 0; i < splitted.length; i++) {
 	xor_codes[i] = char_codes[i] ^ 42;
 }
 
 var result = "";
 
-for (i = 0; i < str.length; i++) {
+for (i = 0; i < splitted.length; i++) {
 	result += String.fromCharCode(xor_codes[i]);
 }
 
-result;
+console.log(result);
