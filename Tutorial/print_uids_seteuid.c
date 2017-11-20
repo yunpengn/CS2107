b3d0ef72749d4fb463dc8949eb1c7afdcac815e6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-20 16:13:46 +0800

    Add two files for Tutorial 7

---
+++ Tutorial/print_uids_seteuid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+	printf("Real user id = %d, effective user id = %d\n", getuid(), geteuid());
+
+	seteuid(1001);
+	printf("Real user id = %d, effective user id = %d\n", getuid(), geteuid());
+
+	setuid(0);
+	printf("Real user id = %d, effective user id = %d\n", getuid(), geteuid());
+	
+	return 0;
+}
